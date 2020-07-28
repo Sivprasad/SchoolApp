7d33017b8b23a26171964f1bbddce98663b08222
Sivaprasad <sivaprasad.g@live.com>
2020-07-28 22:43:05 +0530

    add properties and methods

--- SchoolLibrary/School.cs
+++ SchoolLibrary/School.cs
@@ -6,5 +6,22 @@ namespace SchoolLibrary
 {
     class School
     {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Ciyt { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+
+        //public float AverageThreeScores(float a, float b, float c)
+        //{
+        //    var result = (a + b + c) / 3;
+        //    return result;
+        //}
+        public static float AverageThreeScores(float a, float b, float c) => (a + b + c)/3;
+        public static  int AverageThreeScores(int a, int b, int c)
+        {
+            var result = (a + b + c) / 3;
+            return result;
+        }
     }
 }
